0a0adf52f0dd570e96b796a1e7f16b9e5b75fe13
mauricio5455 <72229381+mauricio5455@users.noreply.github.com>
2021-09-15 21:58:57 -0300

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const samih = JSON.parse(fs.readFileSync('./src/simi.json'))
 const setting = JSON.parse(fs.readFileSync('./src/settings.json'))
 prefix = setting.prefix
 blocked = []
-
+console.log('Bot do ms')
 function kyun(seconds){
   function pad(s){
     return (s < 10 ? '0' : '') + s;
